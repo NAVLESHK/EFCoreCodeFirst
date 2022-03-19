739a554c2ee4a6de2cbd36ff7bd08dbfb659f830
NAVLESHK <100079568+NAVLESHK@users.noreply.github.com>
2022-03-19 18:47:28 +0530

    Add project files.

---
+++ EFCoreCodeFirst/Models/EmployeeContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreCodeFirst.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public  EmployeeContext (DbContextOptions<EmployeeContext> Options) : base(Options)
+        {
+
+        }
+   
+public DbSet<Employee>Employees { get; set; }
+         
+    }
+}
